2b0de8f132ab0a5938dea780171f98b8c9010be4
Emre Yavuzyiğit <EmreTr1@users.noreply.github.com>
2016-06-24 12:43:24 +0300

    v1.0 is Out!

--- src/GameSelector/Main.php
+++ src/GameSelector/Main.php
@@ -1,12 +1,20 @@
 <?php
 
-#GameSelector #Stable3 by EmreTr1
-#Added addcommand command usage!
-#Prefix bug fixed(Â)!
+#  !!!  Version 1.0 !!!
+#  Features:
+#  - Added WaitTask
+#  - Added "list" command(but not working now)
+#  - Support MCPE 0.15.0/1
+#  To-Do(v1.1):
+#  - Add multi-commands to GS Items
+#  - Fix: remove chest screen from player
+#  - Save GS Item on Config!
+ 
 
 namespace GameSelector;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\PluginTask;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
@@ -25,6 +33,7 @@ use pocketmine\math\Vector3;
 use pocketmine\level\particle\FloatingTextParticle;
 use pocketmine\entity\Entity;
 use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\plugin\Plugin;
 use pocketmine\nbt\tag\DoubleTag;
 use pocketmine\nbt\tag\ListTag;
 use pocketmine\nbt\tag\FloatTag;
@@ -49,7 +58,7 @@ class Main extends PluginBase implements Listener{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getServer()->getLogger()->info($this->prefix.c::GREEN."GameSelector has been Enabled!");
 		@mkdir($this->getDataFolder());
-		$this->config = new Config($this->getDataFolder() . "config.yml", Config::JSON);
+		$this->config = new Config($this->getDataFolder() . "Selectors.json", Config::JSON);
 		$this->config->save();
 	}
 	
@@ -57,9 +66,20 @@ class Main extends PluginBase implements Listener{
             $this->getServer()->getLogger()->info($this->prefix.c::RED."GameSelector has been Disabled!");
         }
 	public function OnCommand(CommandSender $s, Command $cmd, $label, array $args){
+		if(empty($args[0])){
+			$s->sendMessage($this->prefix.c::RED."You Need help?: /gs help");
+		}
 		if(!isset($args[0])){unset($sender,$cmd,$label,$args);return false;};
-                if($s->isOp() and $s instanceof Player){
+        if($s->isOp() and $s instanceof Player){
 		switch($args[0]){
+			case "help":
+		        $s->sendMessage(c::GRAY."<---------- $this->prefix ---------->");
+				$s->sendMessage(c::GREEN."/gs add <name>:§d Add the Selector");
+				$s->sendMessage(c::GREEN."/gs additem <SELECTORNAME> <Item ID> <damage>:§d Add item in the Selector");
+				$s->sendMessage(c::GREEN."/gs removeitem <SELECTORNAME> <Item ID> <damage>:§d Remove item from the Selector");
+				$s->sendMessage(c::GREEN."/gs addcommand <SELECTORNAME> <Item ID> <Command(No /)>:§d Add Command to Selector Items");
+				$s->sendMessage(c::GRAY."<---------- $this->prefix ---------->");
+				break;
 			case "add":
 			    if((!empty($args[1])) and !($this->config->getNested("Selectors.$args[1]"))){
 					$this->name=$args[1];
@@ -97,7 +117,7 @@ class Main extends PluginBase implements Listener{
 					$chest->saveNBT();
 					$s->sendMessage($this->prefix.c::RED.$args[2]." ID's item was removed from the ".$args[1]);
 				}else{
-					$s->sendMessage($this->prefix.c::YELLOW."Usage: /gs additem <SELECTORNAME> <Item ID> <damage>");
+					$s->sendMessage($this->prefix.c::YELLOW."Usage: /gs removeitem <SELECTORNAME> <Item ID> <damage>");
 				}
 				break;
 			case "addcommand":
@@ -125,10 +145,22 @@ class Main extends PluginBase implements Listener{
 								$s->sendMessage($this->prefix.c::YELLOW."Usage: /gs addcommand <SELECTORNAME> <Item ID> <Command(No /)>");
 							}
 				break;
+		     case "list":
+			$selectors=$s->getLevel()->getTiles();
+                         if($selectors instanceof Chest){
+                             $cn=$selectors->getName();
+                             if($this->config->getNested("Selectors.$cn")){              // NOT WORKING NOW!
+                                 shuffle($cn);
+			         $s->sendMessage(c::GRAY."<---------- $this->prefix LIST ---------->");
+			         $s->sendMessage($cn);
+			         $s->sendMessage(c::GRAY."<---------- $this->prefix LIST ---------->");
+                             }
+                         }
+			 break;
                         }
 		}else{
                     $s->sendMessage(c::RED."You are not op!");
-                }
+        }
 	}
 	
 	public function OnDamage(EntityDamageEvent $event){
@@ -143,7 +175,7 @@ class Main extends PluginBase implements Listener{
 					$player->getLevel()->setBlock(new Vector3($x, $y, $z), Block::get(54));
                     $chest = new Chest($player->getLevel()->getChunk($x >> 4, $z >> 4, true), new CompoundTag(false, array(new IntTag("x", $x), new IntTag("y", $y), new IntTag("z", $z), new StringTag("id", Tile::CHEST))));
 					$chest->setName($this->name);
-                                        $chest->saveNBT();
+                    $chest->saveNBT();
 			        $player->getLevel()->addTile($chest);
 				   $chest2=new ChestInventory($player->getLevel()->getTile(new Vector3($x, $y, $z)), $player);
 				   $ch=$player->getLevel()->getTile(new Vector3($x, $y, $z));
@@ -178,13 +210,14 @@ class Main extends PluginBase implements Listener{
 	public function InventoryTransactionEvent(InventoryTransactionEvent $event){
 		$Transaction = $event->getTransaction();
 		$Player = null;
-		$BuyingTile = null;
 		$name = null;
+		$chest=null;
 		foreach ($Transaction->getInventories() as $inv) {
 			if ($inv instanceof PlayerInventory)
 				$Player = $inv->getHolder();
 			elseif($inv instanceof ChestInventory)
 				$name = $inv->getHolder()->getName();
+				$chest=$inv->getHolder();
 		}
 		foreach ($Transaction->getTransactions() as $t) {
 			foreach ($this->traderInvTransaction($t) as $nt)
@@ -197,9 +230,11 @@ class Main extends PluginBase implements Listener{
                 $SourceItemid= $SourceItem->getId();
                 if($this->config->getNested("Selectors.$name.Commands.$TargetItemid") and $TargetItem->getId()>=0){
                     $event->setCancelled(true);
-                    $playername=$Player->getName();
+                    //$Player->getInventory()->clearAll();
+                    $Player->removeWindow($chest->getInventory());  // why not working?
                     $command=$this->config->getNested("Selectors.$name.Commands.$TargetItemid");
-                    $this->getServer()->dispatchCommand(new ConsoleCommandSender(), str_ireplace("{player}", $playername, $command));
+                    $playername=$Player->getName();
+                    $this->newTask($playername, $command);
                 }
                 if($this->config->getNested("Selectors.$name.Commands.$SourceItemid") and $SourceItem->getId()>=0){
                     $event->setCancelled(true);
@@ -208,6 +243,12 @@ class Main extends PluginBase implements Listener{
 		
 	}
 	
+        public function newTask($player, $command){
+		$g=new WaitTask($this, $player, $command);
+		$h=$this->getServer()->getScheduler()->scheduleRepeatingTask($g, 20);
+		$g->setHandler($h);
+	}
+        
 	public function traderInvTransaction($t)
 	{
 		$src = clone $t->getSourceItem();
@@ -222,4 +263,33 @@ class Main extends PluginBase implements Listener{
 		}
 		return [];
 	}
+}
+class WaitTask extends PluginTask{
+    
+    public $time=5;
+    private $player;
+    private $command;
+    private $plugin;
+    public $prefix="§8[§dGame§aSelector§8]§r";
+    
+    public function __construct(Plugin $plugin, $player, $command){
+		parent::__construct($plugin);
+		$this->main=$plugin;
+                $this->player=$player;
+                $this->command=$command;
+    }
+    
+    public function OnRun($currentTick){
+        $p=$this->main->getServer()->getPlayer($this->player);
+        if($this->time>=0){
+            $this->time--;
+            $p->sendTip("§aLoading.. $this->time");
+            if($this->time==0){
+                $command=$this->command;
+                $playername=$this->player;
+                $this->main->getServer()->dispatchCommand(new ConsoleCommandSender(), str_ireplace("{player}", $playername, $command));
+                $this->main->getServer()->getScheduler()->cancelTask($this->getTaskId());
+            }
+        }
+    }
 }
